d0d00f877953fcc172e3efaca06f6e1268000023
Taehong Min <W0445964@campus.nscc.ca>
2022-04-17 20:10:47 -0300

    Made onGround  filter functionality

---
+++ FinalProject/FinalProject/FinalProject/FinalProject/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FinalProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        
+        public ViewModels.FlightViewModel cvm { get; set; }
+        public MainPage()
+        {
+            this.InitializeComponent();
+            cvm = new ViewModels.FlightViewModel();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            NoteList.Visibility = Visibility.Visible;
+        }
+
+        private void FilterTextBox_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+            NoteList.Visibility = Visibility.Collapsed;
+        }
+
+    }
+}
---
+++ FinalProject/FinalProject/FinalProject/FinalProject/Models/Flight.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class Flight
+    {
+
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Country { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+        public string Velocity { get; set; }
+        public string OnGround { get; set; }
+
+        public string FlightsAsString => string.Join(",", Country);
+
+        override public string ToString()
+        {
+            return Name + ", " + Number + ", " + Country + ", " + Longitude + ", " + Latitude;
+        }
+
+        public Flight(string name, string num, string country, string lng, string lat, string vel, string onGround)
+        {
+            if(name == "")
+            {
+                Name = "NULL";
+            } else
+            {
+                Name = name;
+            }
+
+            if(num == "")
+            {
+                Number = "NULL";
+            }
+            else
+            {
+                Number = num;
+            }
+
+            if(country == "")
+            {
+                Country = "NULL";
+            }
+            else
+            {
+                Country = country;
+            }
+
+            if(lng == "")
+            {
+                Longitude = "NULL";
+            }
+            else
+            {
+                Longitude = lng;
+            }
+
+            if(lat == "")
+            {
+                Latitude = "NULL";
+            }
+            else
+            {
+                Latitude = lat;
+            }
+
+            if(vel == "")
+            {
+                Velocity = "NULL";
+            }
+            else
+            {
+                Velocity = vel;
+            }
+
+            if (onGround == "")
+            {
+                OnGround = "NULL";
+            }
+            else
+            {
+                OnGround = onGround;
+            }
+
+
+        }
+
+    }
+}
